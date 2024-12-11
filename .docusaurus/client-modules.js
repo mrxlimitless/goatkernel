9b36b4160c771c9dccda839f0bbf0f47ade9f351
mrxlimitless <>
2024-12-11 11:07:56 +0530

    file changes

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/shivankar/shivankar/timepassstuff/goatkernel/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/shivankar/shivankar/timepassstuff/goatkernel/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/shivankar/shivankar/timepassstuff/goatkernel/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/shivankar/shivankar/timepassstuff/goatkernel/src/css/custom.css"),
+];
